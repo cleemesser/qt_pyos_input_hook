ba61cad50acb63f3222b3869c0b954e8f6da7d1f
Chris Lee-Messer <chris.leemesser@gmail.com>
2011-08-20 16:59:20 -0700

    add setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import os
+
+#    requires=["PyQt4 (>4.3)",],
+
+setup(
+    name='qt_pyos_input_hook',
+    version="0.1",
+    author='Chris Lee-Messer',
+    packages=["qt_pyos_input_hook" ],
+    # ext_modules = [] # need to figure out how to hook qmake into an extension?
+    )
