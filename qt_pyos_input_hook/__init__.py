5ba716cbcc5c35356ef83d50b8e656efe1f4abb3
Chris Lee-Messer <chris.leemesser@gmail.com>
2011-08-20 14:39:01 -0700

    add functions to module to add and remove qt inputhook

--- qt_pyos_input_hook/__init__.py
+++ qt_pyos_input_hook/__init__.py
@@ -1,9 +1,12 @@
 import sys,os
 from ctypes import *
-
+basedir = os.path.dirname(__file__)
 if sys.platform[:5] == 'linux':
     pyos = cdll.LoadLibrary(os.path.join(basedir, "libqt_pyos_input_hook.so"))
-    print pyos
-    pyos.qt_pyos_restore_input_hook()
+    qt_pyos_restore_input_hook = pyos.qt_pyos_restore_input_hook
+    qt_pyos_disable_input_hook = pyos.qt_pyos_disable_input_hook
+    
 else:
     raise NotImplemented, "Need to write support for platforms other than linux"
+
+
